3131e02c7070e99e2d9595f001bcc29c1f43971b
Patrick Snowden <patrickbsnowden@outlook.com>
2023-11-08 00:00:43 -0800

    updated z index on mobile headings, added spinner for videos

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -49,18 +49,42 @@ export default function Card({ project }) {
         height={1080}
         alt="thumbnail"
       /> */}
-      <video
-        className="block w-full transition duration-200 relative z-10 rounded-[3px]"
-        width="640"
-        height="360"
-        autoplay="true"
-        playsinline="true"
-        loop
-        muted
-      >
-        <source src={project.video} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <div className="relative rounded-[3px] overflow-hidden select-none aspect-video">
+        <div
+          role="overlay"
+          className="absolute top-0 left-0 w-full h-full flex items-center justify-center rounded-[3px] transition duration-200 mix-blend-difference"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            viewBox="0 0 24 24"
+            class="text-white animate-spin"
+          >
+            <path
+              fill="currentColor"
+              d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
+              opacity=".25"
+            ></path>
+            <path
+              fill="currentColor"
+              d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
+            ></path>
+          </svg>
+        </div>
+        <video
+          className="block w-full transition duration-200 relative"
+          width="640"
+          height="400"
+          autoplay="true"
+          playsinline="true"
+          loop
+          muted
+        >
+          <source src={project.video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
       <div className="col-span-2 space-y-3">
         <h2 className="text-lg font-semibold font-title">{project.name}</h2>
         <p className="">{project.description}</p>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -73,7 +73,7 @@ export default function Home() {
           className="pt-0 lg:pt-20 space-y-5 lg:space-y-0 leading-7"
           id="about"
         >
-          <div className="lg:hidden sticky top-0 -mx-6 py-2 px-6 bg-[#f5f5f5] bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10">
+          <div className="lg:hidden z-50 sticky top-0 -mx-6 py-2 px-6 bg-[#f5f5f5] bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10">
             <h2 className="text-xl font-bold">About</h2>
           </div>
           <div className="space-y-5">
@@ -105,7 +105,7 @@ export default function Home() {
           </div>
         </section>
         <section className="pt-20 space-y-5 lg:space-y-0" id="projects">
-          <div className="lg:hidden sticky top-0 -mx-6 py-2 px-6 bg-[#f5f5f5] bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10">
+          <div className="lg:hidden z-50 sticky top-0 -mx-6 py-2 px-6 bg-[#f5f5f5] bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10">
             <h2 className="text-xl font-bold">Projects</h2>
           </div>
           <div className="space-y-5">
@@ -135,7 +135,7 @@ export default function Home() {
           className="pt-20 space-y-5 lg:space-y-0 min-h-0 lg:min-h-screen"
           id="experience"
         >
-          <div className="lg:hidden sticky top-0 -mx-6 py-2 px-6 bg-[#f5f5f5] bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10">
+          <div className="lg:hidden z-50 sticky top-0 -mx-6 py-2 px-6 bg-[#f5f5f5] bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10">
             <h2 className="text-xl font-bold">Experience</h2>
           </div>
           <div className="space-y-10">
