126d724e95875a0ecccfcd78e5fa14d4a3fa7a6c
Patrick Snowden <patrickbsnowden@outlook.com>
2023-11-07 21:30:48 -0800

    added video to card

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -37,18 +37,33 @@ export default function Card({ project }) {
 
   return (
     <motion.div
-      className="p-3 hover:bg-[#00000008] min-h-[175px] space-y-5 rounded-[6px]"
+      className="p-3 min-h-[175px] space-y-5 rounded-[6px]"
       initial={{ opacity: 0, y: 100 }}
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true }}
     >
-      <Image
+      {/* <Image
         className="rounded-[3px]"
         src={project.image}
         width={1920}
         height={1080}
         alt="thumbnail"
-      />
+      /> */}
+      <video
+        className="block w-full transition duration-200 relative z-10 rounded-[3px]"
+        width="640"
+        height="360"
+        autoplay="true"
+        playsinline="true"
+        loop
+        muted
+      >
+        <source
+          src="https://res.cloudinary.com/hundreddevs/video/upload/v1699421286/statsVideo_lfq7yg.mp4"
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
       <div className="col-span-2 space-y-3">
         <h2 className="text-lg font-semibold font-title">{project.name}</h2>
         <p className="">{project.description}</p>
